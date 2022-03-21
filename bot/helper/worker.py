d78e8c5292b4d36d63f5aae79efb5a2106ab3698
wahieb <wahebtalal@gmail.com>
2022-03-21 21:54:33 +0300

    test

--- bot/helper/worker.py
+++ bot/helper/worker.py
@@ -12,38 +12,48 @@ from os import path
 q = []
 
 
-async def FProgress(current, total,chatid,mesgid):
+async def FProgress(current, total, chatid, mesgid):
     print(f"{current * 100 / total:.1f}%")
     #  print("\r[%-20s] %d%%" % ('=' * int(current * 10 / total),int(current * 100 / total)), end='')
     # proc = "downloading \n" + (
     #        "[%-20s] %.1f%%" % ('=' * (int(current * 20 / total)), (current * 100 / total)))
     #  if str(msg.text) != str(proc):
     try:
-        await  app.edit_message_text(chat_id=chatid,message_id=mesgid,text="downloading \n" + (
+        await  app.edit_message_text(chat_id=chatid, message_id=mesgid, text="downloading \n" + (
                 "[%-20s] %.1f%%" % ('=' * (int(current * 20 / total)), (current * 100 / total))))
     except FloodWait as e:
         print("error download progress")
         await asyncio.sleep(e.x)
 
 
-async def stats(out):
+async def stats(out:str):
     try:
-        ot = os.path.getsize(out)
-        oos = ot / (1024 * 1024)
+        ot = os.path.getsize("encode/"+out.split("-")[0]+"/"+out+ '.HEVC' + '.mp4')
+        oos = hbs(ot)
         ans = "جاري الضغط:" + " \n " + str(oos)
         return ans
     except:
         return "خطأ !!"
 
+def hbs(size):
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "B", 1: "K", 2: "M", 3: "G", 4: "T", 5: "P"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
 
-async def UProgress(current: int, total, chatid,mesgid):
+async def UProgress(current: int, total, chatid, mesgid):
     print(f"{current * 100 / total:.1f}%")
     #  print("\r[%-20s] %d%%" % ('=' * int(current * 10 / total),int(current * 100 / total)), end='')
     #  progress = "uploading \n" + (
     #    "[%-20s] %.1f%%" % ('=' * (int(current * 20 / total)), (current * 100 / total)))
 
     try:
-        await app.edit_message_text(chat_id=chatid,message_id=mesgid,text="uploading \n" + (
+        await app.edit_message_text(chat_id=chatid, message_id=mesgid, text="uploading \n" + (
                 "[%-20s] %.1f%%" % ('=' * (int(current * 20 / total)), (current * 100 / total))))
     except FloodWait as e:
         print("error upload progress")
@@ -60,11 +70,12 @@ async def add_queue(msg: []):
 async def enc(ls: []):
     msg: Message = ls[0]
     file: Message = ls[1]
+
+    print("enc")
+    video_file = ""
     try:
-        print("enc")
-        video_file = ""
         video_file = await file.download(file_name=str(file.chat.id) + "-" + str(file.message_id), progress=FProgress,
-                                         progress_args=(msg.chat.id,msg.message_id))
+                                         progress_args=(msg.chat.id, msg.message_id))
         print(video_file)
         ttl = get_duration(video_file)
         print("ttl  :" + str(ttl))
@@ -73,30 +84,24 @@ async def enc(ls: []):
         thumb = get_thumbnail(video_file, "thumbs//" + str(file.chat.id), 1)
         print("thumb :" + str(thumb))
 
-        enpa = "encode//" + str(file.chat.id)
+        enpa = "encode/" + str(file.chat.id)
         os.makedirs(enpa, exist_ok=True)
         basefilepath, extension = os.path.splitext(video_file)
         print("basefilepath : " + basefilepath + " | extension : " + extension)
         output_filepath = basefilepath + '.HEVC' + '.mp4'
         output_filepath = str(output_filepath).replace("downloads", enpa)
+        print("hihi")
+        await msg.edit(text="Encoding", reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="state", callback_data=str(file.chat.id) + "-" + str(file.message_id))]]))
 
-        try:
-            await msg.edit(text="Encoding", reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton(text="state", callback_data=output_filepath)]]))
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            try:
-                await msg.reply_text(text="Encoding", reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text="state", callback_data=output_filepath)]]))
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-                print("error reply encoding")
-            print("error edit encoding")
+        # await msg.reply_text(text="Encoding", reply_markup=InlineKeyboardMarkup(
+        #   [[InlineKeyboardButton(text="state", callback_data=output_filepath)]]))
+        print("start enc")
         outfile = await encode(video_file, output_filepath)
         try:
             await app.send_video(msg.chat.id, outfile,
                                  progress=UProgress,
-                                 progress_args=(msg.chat.id,msg.message_id)
+                                 progress_args=(msg.chat.id, msg.message_id)
                                  , duration=ttl
                                  , width=width_high[0]
                                  , height=width_high[1]
@@ -119,7 +124,6 @@ async def enc(ls: []):
                 print("error send no progress")
                 await asyncio.sleep(ex.x)
 
-
         try:
             await msg.edit(text="Done!")
         except FloodWait as e:
@@ -133,10 +137,9 @@ async def enc(ls: []):
         os.remove(video_file)
         os.remove(outfile)
         os.remove(thumb)
-    except:
-        print("except")
-        await file.reply_text("Error")
 
+    except:
+        print(32)
     pop()
     if len(q) > 0:
         await enc(q[0])
