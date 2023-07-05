509560becb84d2ea0b4758b1c67d856c18ad222a
wahieb talal <111255131+wahebdev@users.noreply.github.com>
2023-07-05 18:49:14 +0300

    Update __init__.py
    Workers change to 1

--- bot/helper/__init__.py
+++ bot/helper/__init__.py
@@ -18,6 +18,6 @@ try:
     block = str(config("Block", default="1 2")).split()
 except:
     print("error config")
-app = Client("bot", api_id=api, api_hash=hash, bot_token=bot)
+app = Client("bot", api_id=api, api_hash=hash, bot_token=bot,workers=1)
 download_dir = "download"
 codec_dir = "codec"
